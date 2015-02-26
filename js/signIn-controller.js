0c64fa03a222e9476e1ae499afff83722e3bb48f
dreamind <kos.voit@gmail.com>
2015-02-26 20:59:19 +0300

    Save

--- js/signIn-controller.js
+++ js/signIn-controller.js
@@ -15,12 +15,14 @@
 
         var sign    = this,
             tempDB  = new CollectionsDB,
+            isOk    = [0, 0],
+            stop    = [0, 0],
             validList = {
-                oneQ: "Not available \' ",
-                twoQ: "Not available \" ",
-                short: "So short",
-                long:  "So long",
-                empty: "Field must not be empty"
+                Q:      "Not available quotes",
+                short:  "So short",
+                empty:  "Field must not be empty",
+                type:   "Wrong writing",
+                str:    "It is must be string"
             };
 
         // Temp DB:
@@ -33,15 +35,74 @@
         sign.forgot   = false;
         sign.mail     = null;
         sign.pass     = null;
+        sign.errMail  = "";
+        sign.errPass  = "";
 
         // Watchers:
-        $scope.$watch( 'sign.mail', validOnFly );
-        $scope.$watch( 'sign.pass', validOnFly );
+        $scope.$watch( 'sign.mail', validOnFlyMail );
+        $scope.$watch( 'sign.pass', validOnFlyPass );
 
-        // First-level validation:
-        function validOnFly() {
-            console.log( "mail: " + sign.mail );
-            console.log( "pass: " + sign.pass );
+        // First-level validation mail:
+        function validOnFlyMail() {
+            if( !stop[0] ) {
+                stop[0] = 1;
+                return null;
+            }
+
+            var str = sign.mail;
+            if( typeof str === "string" ) {
+                var arrQ = str.match(/[\",\']/ig) || [];
+                console.log( "arrQ: " + arrQ );
+                if( !arrQ.length ) {
+                    if (str.length > 6) {
+                        var arrM = str.match(/[@,.]/ig) || [];
+                        if ( arrM.length ) {
+                            isOk[0] = 1;
+                            sign.errMail = "";
+                        } else {
+                            isOk[0] = 0;
+                            sign.errMail = "Mail: " + validList.type;
+                        }
+                    } else {
+                        isOk[0] = 0;
+                        sign.errMail = "Mail: " + validList.short;
+                    }
+                } else {
+                    isOk[0] = 0;
+                    sign.errMail= "Mail: " + validList.Q;
+                }
+            } else {
+                isOk[0] = 0;
+                sign.errMail = "Mail: " + validList.str;
+            }
+        }
+
+        // First-level validation pass:
+        function validOnFlyPass() {
+            if( !stop[1] ) {
+                stop[1] = 1;
+                return null;
+            }
+
+            var str = sign.pass;
+            if( typeof str === "string" ) {
+                var arrQ = str.match(/[\",\']/ig) || [];
+                if( !arrQ.length ) {
+                    if (str.length > 6) {
+                        isOk[1] = 1;
+                        sign.errPass = "";
+                    } else {
+                        isOk[1] = 0;
+                        sign.errPass = "Password: " + validList.short;
+                    }
+                } else {
+                    isOk[1] = 0;
+                    sign.errPass = "Password: " + validList.Q;
+                }
+            } else {
+                isOk[1] = 0;
+                sign.errPass = "Password: " + validList.str;
+            }
         }
 
         return sign;
